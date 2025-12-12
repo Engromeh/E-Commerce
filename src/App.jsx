58e22adf6c495864546491c068a3ab3a7b4614fc
Mahmoud Saaed Romeh <romeheng1@gmail.com>
2025-12-12 05:49:25 +0200

    Handel Enviroment

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+
+import BtnHeader from './Components/Header/BtnHeader'
+import TopHeader from './Components/Header/TopHeader'
+
+function App() {
+
+  return (
+    <>
+    <header>
+      <TopHeader/>
+      <BtnHeader />
+    </header>
+     
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/Header/BtnHeader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const BtnHeader = () => {
+  return (
+    <div>
+      
+      <h1>BtnHeader</h1>
+    </div>
+  )
+}
+
+export default BtnHeader
